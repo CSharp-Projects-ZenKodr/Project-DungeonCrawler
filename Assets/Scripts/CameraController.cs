3c6dd0c857d86ddb54d85069bfa04dae8b4de0de
CaptainSly <CaptainSly.theonly@gmail.com>
2020-12-09 03:19:12 -0600

    Fuck git bro

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: Zachary Knoblauch
+/// </summary>
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: Zachary Knoblauch
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+
+	public Rigidbody2D body;
+	public Animator animator;
+
+	public float moveSpeed = 5f;
+
+	private Vector2 movement;
+
+	private void Update()
+	{
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+
+		if (movement != Vector2.zero)
+		{
+			animator.SetFloat("Horizontal", movement.x);
+			animator.SetFloat("Vertical", movement.y);
+		}
+
+		animator.SetFloat("Speed", movement.sqrMagnitude);
+	}
+
+	private void FixedUpdate()
+	{
+		body.MovePosition(body.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+
+}
